e470624f411f0d959126586b790189720b4897e3
“KakaMbweha01” <leonestill804@gmail.com>
2024-03-21 14:25:03 +0300

    raund2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: produces a string of words.
- * Return: Always 0 (Success)
- */
+*main - Entry point
+*
+*Description: produces a string of words.
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art, ");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
 *main - Entry point
 *
-*Description: Produces the size of various types on a pc its compiled in.
-*Return: Always (Success)
+*Description:Produces the size of various types on a pc its compiled in.
+*Return:Always (Success)
 */
 int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
-printf("Size of a int: %zu byte(s)\n", sizeof(int));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-return(0);
+return (0);
 }
