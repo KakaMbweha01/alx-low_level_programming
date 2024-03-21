d997ec3ecc5c854acb1332bb2818bc9538818557
“KakaMbweha01” <leonestill804@gmail.com>
2024-03-21 13:46:51 +0300

    reformed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
+/*
+*main-Entry point
+*
+*Description - Writes a program to produce a string of words
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
